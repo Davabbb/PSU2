fca9b7521bc899f9f8a2eaf2b5d9108f8145222d
David Beylin <57668741+davabey@users.noreply.github.com>
2021-11-24 23:25:37 +0500

    5_1

---
+++ 5_1/main.cpp
@@ -0,0 +1,33 @@
+//https://contest.yandex.ru/contest/30914/run-report/59022659/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+void msd_sort(std::vector<std::string> * data, int index, std::vector<int>* data_help) {
+    std::map <char, std::vector<int>> count;
+    for (int s : *data_help) {
+        if ((*data)[s].size() > index) {
+            count[(*data)[s][index]].push_back(s);
+        } else {
+            std::cout << (*data)[s] << std::endl;
+        }
+    }
+    for (auto& i : count) {
+        msd_sort(data, index + 1, &i.second);
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    std::vector<std::string> data;
+    for(std::string s; std::cin >> s; data.push_back(s));
+    std::vector<int> data_help;
+    data_help.reserve(data.size());
+    for (int i = 0; i < data.size(); i++) data_help.push_back(i);
+    msd_sort(&data, 0, &data_help);
+    return 0;
+}
