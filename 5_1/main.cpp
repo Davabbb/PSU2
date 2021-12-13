5e89a3b2e5bad4c3352ab50ab704a0378ca40b63
David Beylin <57668741+davabey@users.noreply.github.com>
2021-12-13 23:30:22 +0500

    всё исправил

--- 5_1/main.cpp
+++ 5_1/main.cpp
@@ -1,21 +1,26 @@
-//https://contest.yandex.ru/contest/30914/run-report/59097692/
+//https://contest.yandex.ru/contest/30914/run-report/61024788/
 
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
 
-void msd_sort(std::vector<std::string>* data, int index, std::vector<int>* data_help) {
+void msd_sort(std::vector<std::string>& data, int index, std::vector<int>& data_help,
+              std::vector<std::string>& new_data) {
     std::map <char, std::vector<int>> count;
-    for (int s : *data_help) {
-        if ((*data)[s].size() > index) {
-            count[(*data)[s][index]].push_back(s);
+    for (int s : data_help) {
+        if (data[s].size() > index) {
+            count[data[s][index]].push_back(s);
         } else {
-            std::cout << (*data)[s] << std::endl;
+            new_data.push_back(data[s]);
         }
     }
     for (auto& i : count) {
-        msd_sort(data, index + 1, &i.second);
+        if (i.second.size() != 1) {
+            msd_sort(data, index + 1, i.second, new_data);
+        } else {
+            new_data.push_back(data[i.second[0]]);
+        }
     }
 }
 
@@ -23,11 +28,15 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
+    std::vector<int> data_help;
+    std::vector<std::string> new_data;
     std::vector<std::string> data;
     for(std::string s; std::cin >> s; data.push_back(s));
-    std::vector<int> data_help;
     data_help.reserve(data.size());
     for (int i = 0; i < data.size(); i++) data_help.push_back(i);
-    msd_sort(&data, 0, &data_help);
+    msd_sort(data, 0, data_help, new_data);
+    for (auto & i : new_data) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
